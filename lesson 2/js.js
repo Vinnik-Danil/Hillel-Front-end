d45d9879226b04785800e6adc6aebc176b85f123
danik <daniilvinnik@ukr.net>
2021-01-24 23:14:25 +0200

    beta1

--- lesson 2/js.js
+++ lesson 2/js.js
@@ -6,21 +6,21 @@ number2 = Number(number2);
 
 let function_type = prompt("Выберете тип операции \n+ \n-\n* \n/");
     if(function_type=='+'){
-        alert('Результат: ' + Number(number1+number2));
+        alert(number1 + "+" + number2+ "=" + Number(number1+number2));
     }
     if(function_type=='-'){
-        alert('Результат: ' + Number(number1-number2));
+        alert(number1 + "-" + number2 + "=" + Number(number1-number2));
     }
     if(function_type=='*'){
-        alert('Результат: ' + Number(number1*number2));
+        alert(number1 + "*" + number2+ "=" + Number(number1*number2));
     }
     if(function_type=='/'){
         if(number2==0){
             alert('На 0 делить запрещено');
-        
+            
         }
-        alert('Результат: ' + Number(number1/number2));
+        alert(number1 + "/" + number2+ "=" + Number(number1/number2));
     }
-
+    
 
 
