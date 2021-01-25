33620713052615dab7553b9ab3fff40d86de9726
danik <daniilvinnik@ukr.net>
2021-01-25 13:08:29 +0200

    beta2

--- lesson 2/js.js
+++ lesson 2/js.js
@@ -7,18 +7,22 @@ number2 = Number(number2);
 let function_type = prompt("Выберете тип операции \n+ \n-\n* \n/");
     if(function_type=='+'){
         alert(number1 + "+" + number2+ "=" + Number(number1+number2));
+        console.log(number1+number2);
     }
     if(function_type=='-'){
         alert(number1 + "-" + number2 + "=" + Number(number1-number2));
+        console.log(number1-number2);
     }
     if(function_type=='*'){
         alert(number1 + "*" + number2+ "=" + Number(number1*number2));
+        console.log(number1*number2);
     }
     if(function_type=='/'){
         if(number2==0){
             alert('На 0 делить запрещено');
             
         }
+        console.log(number1/number2);
         alert(number1 + "/" + number2+ "=" + Number(number1/number2));
     }
     
