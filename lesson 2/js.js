a5fba05f80d893772a385c899823b277aa752605
danik <daniilvinnik@ukr.net>
2021-01-28 19:38:15 +0200

    home work 2 (beta 2)

--- lesson 2/js.js
+++ lesson 2/js.js
@@ -3,6 +3,7 @@ let number1 = prompt("Введите число 1");
 number1 = Number(number1);
 let number2 = prompt("Введите число 2");
 number2 = Number(number2);
+let result;
 // const result = "Result operation: \n";
 
 // let function_type = prompt("Выберете тип операции \n+ \n-\n* \n/");
@@ -26,11 +27,23 @@ number2 = Number(number2);
 //         console.log(number1/number2);
 //         alert(number1 + "/" + number2+ "=" + (number1/number2));
 //     }
-    const result =`Result operation
-    Operation[+]: ${number1} + ${number2} = ${number1+number2} 
-    Operation[-]: ${number1} - ${number2} = ${number1-number2} 
-    Operation[*]: ${number1} * ${number2} = ${number1*number2} 
-    Operation[/]: ${number1} / ${number2} = ${number1/number2} 
+    const add = function ($number1, $number2){
+        return number1 + number2;   
+    }
+    const diff = function ($number1, $number2){
+        return number1 - number2;   
+    }
+    const mult = function ($number1, $number2){
+        return number1 * number2;   
+    }
+    const div = function ($number1, $number2){
+        return number1 / number2;   
+    }
+    result =`Result operation
+    Operation[+]: ${number1} + ${number2} = ${add()} 
+    Operation[-]: ${number1} - ${number2} = ${diff()} 
+    Operation[*]: ${number1} * ${number2} = ${mult()} 
+    Operation[/]: ${number1} / ${number2} = ${div()} 
     `
     alert(result);
     console.log(result);
