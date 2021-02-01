d1141a636276d25353aa3dcfd89245762f23b44e
danik <daniilvinnik@ukr.net>
2021-02-01 15:32:23 +0200

    Beta4

--- lesson 3/js.js
+++ lesson 3/js.js
@@ -1,26 +1,46 @@
 alert("Привет!");
 let number1 = prompt("Введите число 1");
-number1 = Number(number1);
 let number2 = prompt("Введите число 2");
+number1 = Number(number1);
 number2 = Number(number2);
+
+// function count(num1, num2, mark) {
+//     switch (mark) {
+//       case "*":
+//         return num1 * num2;
+//       case "+":
+//         return num1 + num2;
+//       case "-":
+//         return num1 - num2;
+//       case "/":
+//         if (num2 !== 0) {
+//           return num1 / num2;
+//         } else {
+//           return "не делится на нуль";
+//         }
+//     }
+//   }
+
 let result;
-    const add = function (){
-        return number1 + number2;   
+    const add = function (a, b){
+        return a+b;
     }
-    const diff = function (){
-        return number1 - number2;   
+    const diff = function (a, b){
+        return a-b;  
     }
-    const mult = function (){
-        return number1 * number2;   
+    const mult = function (a, b){
+        return a*b;
     }
-    const div = function (){
-        return number1 / number2;   
+    const div = function (a, b){
+        return a/b;
     }
+
+
     result =`Result operation
-    Operation[+]: ${number1} + ${number2} = ${add()} 
-    Operation[-]: ${number1} - ${number2} = ${diff()} 
-    Operation[*]: ${number1} * ${number2} = ${mult()} 
-    Operation[/]: ${number1} / ${number2} = ${div()} 
+    Operation[+]: ${number1} + ${number2} = ${add(number1, number2)} 
+    Operation[-]: ${number1} - ${number2} = ${diff(number1, number2)} 
+    Operation[*]: ${number1} * ${number2} = ${mult(number1, number2)} 
+    Operation[/]: ${number1} / ${number2} = ${div(number1, number2)} 
     `
     alert(result);
     console.log(result);
