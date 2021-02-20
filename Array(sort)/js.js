7f86e497c98f26c910ac58da098d7d199f37e383
Vinnik-Danil <58749648+Vinnik-Danil@users.noreply.github.com>
2021-02-20 21:34:37 +0200

    beta1

--- Array(sort)/js.js
+++ Array(sort)/js.js
@@ -1,48 +1,50 @@
 let arr = new Array();
-let arrSort =new Array();
-let number;
-let i=0;
-number = Number(number);
+let arrSort = new Array();
 
 function sort(choose) {
-let result = true;
-let index;
-while(arr.length<4 || result){
-    number = prompt("Введите число ");
-    if(number==null || number==''){
-        result= false;
-        break;
-    }
-    arr[i] = number;
-    i++;
-    if (!isFinite(number)){
-        alert("Вы ввели не число повторите операцию");
-        i--;
-    }
-    console.log(arr.toString());  
-}
-alert("Введенный массив \n" + arr.toString());
-if(operation == 1){
-    for (i=0; i<arr.length; i++){
-        for (j=0; j<arr.length; j++){
-            if (arr[i]<arr[j]){
-                index = arr[i];
-                arr [i]= arr [j];
-                arr [j]= index;} 
+    let number;
+    number = Number(number);
+    let i = 0;
+    let result = true;
+    let index;
+    while (arr.length < 4 || result) {
+        number = prompt("Введите число ");
+        if (number == null || number == '') {
+            result = false;
+            break;
         }
-    }   
-}else{
-    for (i=0; i<arr.length; i++){
-        for (j=0; j<arr.length; j++){
-            if (arr[i]>arr[j]){
-                index = arr[i];
-                arr [i]= arr [j];
-                arr [j]= index;} 
+        arr[i] = number;
+        i++;
+        if (!isFinite(number)) {
+            alert("Вы ввели не число повторите операцию");
+            i--;
+        }
+        console.log(arr.toString());
+    }
+    alert("Введенный массив \n" + arr.toString());
+    if (operation == 1) {
+        for (i = 0; i < arr.length; i++) {
+            for (j = 0; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    index = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = index;
+                }
+            }
+        }
+    } else {
+        for (i = 0; i < arr.length; i++) {
+            for (j = 0; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    index = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = index;
+                }
+            }
         }
     }
-}
 
-alert("Отсортированый массив \n" + arr.toString());
+    alert("Отсортированый массив \n" + arr.toString());
 }
 
 let operation = prompt("1-По возрастанию\n2-По убыванию\n");
