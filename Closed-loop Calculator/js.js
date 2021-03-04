8edec70361196098561e0ff197583aa6d8c21802
Vinnik-Danil <58749648+Vinnik-Danil@users.noreply.github.com>
2021-03-04 19:23:58 +0200

    Понял уже на занятии что скорей всего сделал еправильно(

--- Closed-loop Calculator/js.js
+++ Closed-loop Calculator/js.js
@@ -1,9 +1,47 @@
-function sum(numb1,numb2) {
-    return function() {
+function sum(numb1) {
+    return function add(numb2) {
         console.log(numb1+ numb2);
         return numb1+numb2;
     };
 }
-let fn = sum(1,6);
-alert(fn());
+
+
+
+function diff(numb1,numb2) {
+    return function() {
+        console.log(numb1-numb2);
+        return numb1-numb2;
+    };
+}
+
+
+
+function mult(numb1,numb2) {
+    return function() {
+        console.log(numb1*numb2);
+        return numb1*numb2;
+    };
+}
+
+
+function div(numb1,numb2) {
+    return function() {
+        console.log(numb1/numb2);
+        return numb1/numb2;
+    };
+}
+
+
+
+let sum1 = sum(1,6);
+alert(sum1());
+
+// let diff1 = diff(1,6);
+// alert(diff1());
+
+// let mult1 = mult(1,6);
+// alert(mult1());
+
+// let div1 = div(1,6);
+// alert(sum1());
 
