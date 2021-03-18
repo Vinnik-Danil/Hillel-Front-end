ca6f70202330f5b03c34b17cadaf9a66cd7e6ba5
Vinnik-Danil <58749648+Vinnik-Danil@users.noreply.github.com>
2021-03-18 17:24:35 +0200

    beta1

--- Context/js.js
+++ Context/js.js
@@ -2,30 +2,79 @@ const students = [
     {
         name: 'Mark',
         marks: [100, 65, 88, 90, 50],
-        attends: 0
+        attends: 0,
+        avgMark: getAvgMark,
+        maxMark: getMaxMark,
+        setAttend: setAttend,
+        getInfo: getInfo
     },
     {
         name: 'John',
         marks: [83, 10, 65, 0, 50, 83],
-        attends: 0
+        attends: 0,
+        avgMark: getAvgMark,
+        maxMark: getMaxMark,
+        setAttend: setAttend,
+        getInfo: getInfo
     },
     {
         name: 'Joel',
         marks: [100, 65, 90, 20],
-        attends: 0
+        attends: 0,
+        avgMark: getAvgMark,
+        maxMark: getMaxMark,
+        setAttend: setAttend,
+        getInfo: getInfo
     }
 ];
 
 
-function getAvgMark() { } // средняя оценка студента
+function getAvgMark() { 
+    let avg = 0;
+    let len = this.marks.length;
+    for (let i = 0; i < this.marks.length; i++) {
+        avg = avg + this.marks[i];
+    } 
+    avg = avg/this.marks.length;
+    return avg;
+} // средняя оценка студента
 
 
-function getMaxMark() { } // максимальна оценка студента
+function getMaxMark() { 
+    let max=0;
+    for (let i = 0; i < this.marks.length; i++) {
+        if(max<this.marks[i]){
+            max = this.marks[i];
+        }
+        
+    }
+    return max;
+} // максимальна оценка студента
 
 
-function setAttend() {} // увличить счетчик посещений студента
+function setAttend() {
+    this.attends++;
+} // увличить счетчик посещений студента
 
 
 function getInfo() {
+    return `Имя: ${this.name} \nСредняя оценка ${this.avgMark()}\nКоличество посещений ${this.attends}\n`
     
 } // возвращаем строку с именем студента, средней оценкой и колличеством посещений
+
+
+// students.map((e) => {
+//   console.log(`Средняя оценка cтудента ${e.name}: ${e.avgMark()}`);
+// });
+
+// students.map((e) => {
+//     console.log(`Максимальная оценка cтудента ${e.name}: ${e.maxMark()}`);
+//   });
+
+students.map((e) =>{ 
+     e.setAttend();
+    console.log(`Посещение студента ${e.name} увеличено на 1/n количество посещейний =${e.attends}`);
+});
+students.map((e) =>{ 
+    console.log(e.getInfo());
+});
